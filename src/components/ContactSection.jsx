1baf3b8f3b2887240d6b303eb27a90aa7cbf6761
Leonard Ramos <ramos27leonard@gmail.com>
2025-12-16 07:30:19 +0800

    Fixed spacing and margins in Contact section

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -3,13 +3,13 @@ import SlideIn from "./SlideIn";
 
 const ContactSection = () => {
     return (
-        <section className="mb-[60px]">
+        <section className="mb-16">
             <SlideIn 
                 direction="left"
                 delay={0.3}
             >
                 <hr className="border-slate-300"/>
-                <h2 className="text-2xl font-bold mt-10">Let's Get in Touch</h2>
+                <h2 className="text-2xl font-bold mt-16">Let's Get in Touch</h2>
                 <div className="mt-4 md:flex md:gap-24">
                     <p className="text-base">Feel free to reach out if you're interested with working with me. You can contact me via email at <a href="mailto:ramos27leonard@gmail.com" className="text-green hover:text-dark-green transition-colors font-bold">ramos27leonard@gmail.com</a>.</p>
                     <div className="max-w-fit mt-4 md:mt-0">
