834c0b9599cbbd78db5dbc45a2a769af86f27e5b
Leonard Ramos <ramos27leonard@gmail.com>
2025-12-16 07:29:14 +0800

    Fixed spacing and margin issues in main and about section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,30 @@ import ProjectsSection from "./components/ProjectsSection"
 const projects = [
   { 
     id: 1,
+    title: "Weather App",
+    photo: "Weather-App.png",
+    tools: ["Nextjs", "React", "Tailwind", "Typescript", "Shadcn"],
+    desc: "A responsive weather application featuring search functionality, unit conversion, and detailed forecasts from the Open-Meteo API.",
+    frontendmentor: true,
+    challengelink: "https://www.frontendmentor.io/challenges/weather-app-K1FhddVm49",
+    site: "https://weather-now-app-nextjs.netlify.app/",
+    code: "https://github.com/leonard-ramos27/weather-app-nextjs.git",
+    gridRow: "grid-row-md",
+  },
+  { 
+    id: 2,
+    title: "Translate App",
+    photo: "Translate-App.png",
+    tools: ["React", "Tailwind", "Typescript"],
+    desc: "A responsive translation app that connects to a third-party API to translate text between different languages and uses debouncing for real-time update.",
+    frontendmentor: false,
+    challengelink: "",
+    site: "https://translated-io-react.netlify.app/",
+    code: "https://github.com/leonard-ramos27/Translate-App-React.git",
+    gridRow: "grid-row-md",
+  },
+  { 
+    id: 3,
     title: "Multi-Step Form",
     photo: "Multi-Step-Form.png",
     tools: ["React", "Bootstrap", "Typescript"],
@@ -16,7 +40,7 @@ const projects = [
     gridRow: "grid-row-md",
   },
   { 
-    id: 2,
+    id: 4,
     title: "Interactive Comments Section",
     photo: "Interactive-Comments-Section.png",
     tools: ["React", "Bootstrap"],
@@ -26,48 +50,12 @@ const projects = [
     site: "https://leonard-ramos27.github.io/Interactive-Comments-Section-using-ReactJS/",
     code: "https://github.com/leonard-ramos27/Interactive-Comments-Section-using-ReactJS.git",
     gridRow: "grid-row-xl",
-  },
-  { 
-    id: 3,
-    title: "Advice Generator App",
-    photo: "Advice-Generator-App.png",
-    tools: ["React", "Tailwind"],
-    desc: "An app that connects to an API to display a random advice on each click.",
-    frontendmentor: true,
-    challengelink: "https://www.frontendmentor.io/challenges/advice-generator-app-QdUG-13db",
-    site: "https://leonard-ramos27.github.io/Advice-Generator-App/",
-    code: "https://github.com/leonard-ramos27/Advice-Generator-App.git",
-    gridRow: "grid-row-xs",
-  },
-  { 
-    id: 4,
-    title: "Shortly Landing Page",
-    photo: "Shortly-Landing-Page-mini.png",
-    tools: ["HTML", "CSS", "Javascript"],
-    desc: "A landing page that connects to an API to let users create a shortened version of any link.",
-    frontendmentor: true,
-    challengelink: "https://www.frontendmentor.io/challenges/url-shortening-api-landing-page-2ce3ob-G",
-    site: "https://leonard-ramos27.github.io/URL-Shortening-API-Landing-Page/",
-    code: "https://github.com/leonard-ramos27/URL-Shortening-API-Landing-Page.git",
-    gridRow: "grid-row-lg",
-  },
-  { 
-    id: 5,
-    title: "E-commerce Product Page",
-    photo: "Ecommerce-Product-Page.png",
-    tools: ["JQuery", "Bootstrap"],
-    desc: "A page that displays details of a product and lets users add and remove the item in a cart.",
-    frontendmentor: true,
-    challengelink: "https://www.frontendmentor.io/challenges/ecommerce-product-page-UPsZ9MJp6",
-    site: "https://leonard-ramos27.github.io/Ecommerce-Product-Page/",
-    code: "https://github.com/leonard-ramos27/Ecommerce-Product-Page.git",
-    gridRow: "grid-row-sm",
-  },
+  }
 ]
 
 function App() {
   return (
-    <main className="max-w-[800px] mx-6 md:mx-auto">
+    <main className="lg:max-w-[800px] mx-6 md:mx-10 lg:mx-auto">
       <AboutSection />
       <ProjectsSection projects={projects}/>
       <ContactSection />
--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -6,42 +6,40 @@ import Header from "./Header";
 
 const AboutSection = () => {
     return (
-        <section className="min-h-screen pt-16 pb-9 mb-4 flex flex-col justify-center items-center gap-5">
+        <section className="min-h-screen pt-16 pb-9 mb-11 flex flex-col justify-center items-center gap-5">
             <SlideIn direction="left" styles="flex-grow flex flex-col justify-between items-stretch gap-16">
                 <Header />
-                <div className="flex-grow flex flex-col justify-between items-center">
-                    <div>
-                        <h1 className="mb-3 text-black">
-                            Kumusta! I'm Leonard from the Philippines 👋🏼
-                        </h1>
-                        <p className="mb-3">
-                            I specialize in building responsive and user-friendly web applications that deliver smooth and engaging experiences. I enjoy transforming ideas and designs into clean, functional user interfaces.
-                        </p>
-                        <p className="mb-2">Tools and technologies I work with:</p>
-                        <div className="flex flex-row flex-wrap justify-start items-start gap-x-4 gap-y-2 mb-6">
-                            <span className="tool-icon-hover react"><SiReact /> React</span>
-                            <span className="tool-icon-hover svelte"><SiSvelte /> Svelte</span>
-                            <span className="tool-icon-hover typescript"><SiTypescript /> Typescript</span>
-                            <span className="tool-icon-hover tailwind"><SiTailwindcss /> Tailwind</span>
-                            <span className="tool-icon-hover bootstrap"><SiBootstrap /> Bootstrap</span>
-                            <span className="tool-icon-hover nextjs"><SiNextdotjs /> Next.js</span>
-                            <span className="tool-icon-hover django"><SiDjango /> Django</span>
-                            <span className="tool-icon-hover redux"><SiRedux /> Redux</span>
-                            <span className="tool-icon-hover swr"><SiSwr /> SWR</span>
-                            <span className="tool-icon-hover jquery"><SiJquery /> jQuery</span>
-                            <span className="tool-icon-hover shadcn"><SiShadcnui /> Shadcn-ui</span>
-                            <span className="tool-icon-hover vite"><SiVite /> Vite</span>
-                            <span className="tool-icon-hover figma"><SiFigma /> Figma</span>
-                        </div>
+                <div className="-mt-4">
+                    <h1 className="mb-3 text-black">
+                        Kumusta! I'm Leonard from the Philippines 👋🏼
+                    </h1>
+                    <p className="mb-3">
+                        I specialize in building responsive and user-friendly web applications that deliver smooth and engaging experiences. I enjoy transforming ideas and designs into clean, functional user interfaces.
+                    </p>
+                    <p className="mb-2">Tools and technologies I work with:</p>
+                    <div className="flex flex-row flex-wrap justify-start items-start gap-x-4 gap-y-2 mb-2 md:mb-6">
+                        <span className="tool-icon-hover react"><SiReact /> React</span>
+                        <span className="tool-icon-hover svelte"><SiSvelte /> Svelte</span>
+                        <span className="tool-icon-hover typescript"><SiTypescript /> Typescript</span>
+                        <span className="tool-icon-hover tailwind"><SiTailwindcss /> Tailwind</span>
+                        <span className="tool-icon-hover bootstrap"><SiBootstrap /> Bootstrap</span>
+                        <span className="tool-icon-hover nextjs"><SiNextdotjs /> Next.js</span>
+                        <span className="tool-icon-hover django"><SiDjango /> Django</span>
+                        <span className="tool-icon-hover redux"><SiRedux /> Redux</span>
+                        <span className="tool-icon-hover swr"><SiSwr /> SWR</span>
+                        <span className="tool-icon-hover jquery"><SiJquery /> jQuery</span>
+                        <span className="tool-icon-hover shadcn"><SiShadcnui /> Shadcn-ui</span>
+                        <span className="tool-icon-hover vite"><SiVite /> Vite</span>
+                        <span className="tool-icon-hover figma"><SiFigma /> Figma</span>
                     </div>
-                    <div>
-                        <a href="#projects-section" className="flex flex-col items-center justify-center">
-                            <Bounce>
-                                <IoArrowDownCircleOutline className="text-[2rem]"/>
-                            </Bounce>
-                            <div>Scroll down to view some of my work</div>
-                        </a>
-                    </div>
+                </div>
+                <div>
+                    <a href="#projects-section" className="flex flex-col items-center justify-center">
+                        <Bounce>
+                            <IoArrowDownCircleOutline className="text-[2rem]"/>
+                        </Bounce>
+                        <div>Scroll down to view some of my work</div>
+                    </a>
                 </div>
             </SlideIn>
         </section>
